a3f6400bbbff46bcbd4a0b94589f4dc12b806dc0
Emon Parvez <32875754+emonparvez@users.noreply.github.com>
2022-08-08 22:43:46 +0200

    Add files via upload
    How to create a function and return value in python is done here.

---
+++ function.py
@@ -0,0 +1,14 @@
+
+# Defining Main Function
+def main():
+
+    name = input("Whats your name?")
+    hello(name)
+
+
+def hello(to):
+
+    print("Hello", to)
+
+
+main()
---
+++ return.py
@@ -0,0 +1,10 @@
+def main():
+    x = int(input("Whats X?"))
+    print("X Squared is", square(x))
+
+
+def square(n):
+    return n*n
+
+
+main()
