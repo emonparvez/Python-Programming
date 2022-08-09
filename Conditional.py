d9fc47dd0873c65b5ae04854d7dd896017f40cf6
Emon Parvez <32875754+emonparvez@users.noreply.github.com>
2022-08-09 23:29:20 +0200

    Add files via upload
    Conditional Operator Using if and elif

---
+++ Conditional.py
@@ -0,0 +1,15 @@
+#Comparing two number using if statement
+
+x=int(input("Whats X?"))
+y=int(input("Whats y?"))
+
+if x>y:
+    print("X is greater than y")
+
+if x<y:
+    print("Y is greater than X")
+
+if x==y:
+    print("X is equal to Y")
+
+
